54f73d56eb44f9316e0328032d9b92c0b78aaa65
Eli Holmes <eli.holmes@noaa.gov>
2017-03-29 19:53:06 -0700

    use only.logLik and return.lag.one used in function call
    in MARSSkfas call

--- R/MARSSkf.r
+++ R/MARSSkf.r
@@ -9,6 +9,6 @@ if(is.null(MLEobj$par))
 if(MLEobj$fun.kf=="MARSSkfss") 
   return(MARSSkfss(MLEobj))
 if(MLEobj$fun.kf=="MARSSkfas") 
-  return( MARSSkfas(MLEobj, only.logLik=FALSE, return.lag.one=TRUE, return.kfas.model=FALSE ) )
+  return( MARSSkfas(MLEobj, only.logLik=only.logLik, return.lag.one=return.lag.one, return.kfas.model=return.kfas.model ) )
 return(list(ok=FALSE, errors="kf.function does not specify a valid Kalman filter and smoother function."))
 }
