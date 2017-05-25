4facac9803a1592f58a3707cd347e7fbb1ad708f
Eli Holmes <eli.holmes@noaa.gov>
2017-05-25 12:43:55 -0700

    added various elements
    * hessian.fun argument for Harvey1989, fdHess or optim
    * some extraneous code in assignment of par.bias, par.se etc removed
    * all code for compute parSigma removed and put in MARSShessian
    * cleaned up warnings
    * cleaned up code that dealt with NAs
    * changed info re how CIs computed to a list

--- R/MARSSparamCIs.R
+++ R/MARSSparamCIs.R
@@ -1,143 +1,42 @@
 ####################################################################################
 #   MARSSparamCIs function
 #   This returns CIs for ML parameter estimates
-#   If method='hessian', uses the Harvey 1989 algorithm 
-#     which gives an analytical solution to the hessian.  Numerical estimate, possible too.
-#   If method='numerical', it uses nlme::fdHess in MARSShessian.chol(), called by MARSSFisherI(..., method="numerical")
-#     the Hessian for the variance will be for the chol transformed var-cov matrices,
-#     so a back transformation is needed.  If the matrix is diagonal, then there is 
-#     an analytical solution using the 1-4 moments.  If the matrix is unconstrained, #     then the s.e. and CIs are obtained by simulating from the Hessian.
+#   If method='hessian', uses either Harvey1989 (analytical) or fdHess (numerical) or optim (numerical)
 ####################################################################################
-MARSSparamCIs = function(MLEobj, method="hessian", alpha=0.05, nboot=1000, silent=TRUE) {
+MARSSparamCIs = function(MLEobj, method="hessian", alpha=0.05, nboot=1000, silent=TRUE, hessian.fun="Harvey1989") {
   #this function expects a marssMLE object
-  #it will add some vectors of standard errors, biases, low/up CIs to MLEobj
-  if(!(method %in% c("hessian","parametric","innovations","hessian-numerical", "hessian-harvey1989"))) stop("Stopped in MARSSparamCIs(). Current methods are hessian, parametric, innovations, hessian-numerical and hessian-harvey1989.\n", call.=FALSE)
+  #it will add standard errors, biases, low/up CIs to MLEobj
   if(!is.marssMLE(MLEobj)) stop("Stopped in MARSSparamCIs(). This function needs a valid marssMLE object.\n", call.=FALSE)
+
+    if(!(method %in% c("hessian","parametric","innovations"))) stop("Stopped in MARSSparamCIs(). Current methods are hessian, parametric, innovations.\n", call.=FALSE)
+  if(!(hessian.fun %in% c("Harvey1989","fdHess","optim"))) stop("Stopped in MARSSparamCIs(). Available functions for computing the Hessian are Harvey1989, fdHess, and optim.\n", call.=FALSE)  
+  
+  
   paramvec = MARSSvectorizeparam(MLEobj)
   if(length(paramvec)==0) stop("Stopped in MARSSparamCIs(). No estimated parameter elements.\n", call.=FALSE)
-  paramnames=names(paramvec)
-  if(method=="hessian"){ #choose method from MLEobj method
-    #need package globals
-    kem.methods=get("kem.methods", envir=pkg_globals)
-    optim.methods=get("optim.methods", envir=pkg_globals) 
-    
-    if(MLEobj$method%in%kem.methods) method="hessian-harvey1989"
-    if(MLEobj$method%in%optim.methods) method="hessian-numerical"
-  }
-  if(method=="hessian-numerical")  {
-    #Only for fixed, diagonal or block-diagonal unconstrained R, Q, or V0
-    RQV0type=ok.form=c(Q=NA, R=NA, V0=NA)
-    tmp=coef(MLEobj, type="matrix")
-    for(elem in c("R","Q","V0")){
-      ok.form[elem] = is.validvarcov(tmp[[elem]], method="BFGS")$ok
-      if(!ok.form[elem]){ RQV0type[elem]="illegal"; next }
-      if(length(MLEobj$par[[elem]])==0){ RQV0type[elem]="fixed"; next }  
-      if(is.diagonal(tmp[[elem]])){ RQV0type[elem]="diagonal"; next } 
-      #if legal, then either fixed, diagonal or block unconstrained
-      RQV0type[elem]="unconstrained"
-    }
-    if(any(!ok.form)) stop("MARSSparamCIs: To use method=hessian-numerical, R, Q, and V0 must be either fixed, diagonal or block diagonal unconstrained.")
-    #Run nlme::fdHess to get hessian
-    #MLEobj that is returned has var-cov matrices chol transformed
-    MLEobj.hessian = MARSSFisherI(MLEobj, method="numerical")
-    #deal with NAs in the Hessian
-    na.diag = is.na(diag(MLEobj.hessian$Hessian))
-    diag(MLEobj.hessian$Hessian)[ na.diag ] = 1 #this will lead to NA as needed
-    MLEobj.hessian$Hessian[is.na(MLEobj.hessian$Hessian)]=0
-    
-    #standard errors
-    hessInv = try(solve(MLEobj.hessian$Hessian))
-    vector.par.se =rep(NA,length(paramvec)); names(vector.par.se)=names(paramvec)
-    if(inherits(hessInv, "try-error") ) {
-      warning("MARSSparamCIs: Hessian cannot be inverted; stderr = NA is being returned")
-    }else{
-      diag(hessInv)[ na.diag ] = 0
-      is.neg=diag(hessInv)<0
-      vector.par.se[!is.neg] = try(sqrt(diag(hessInv)[!is.neg]), silent=TRUE)
-      #invert the Hessian; wrap in a try() in case it fails
-      if(inherits(vector.par.se, "try-error") || any(is.nan(vector.par.se))) {
-        warning("MARSSparamCIs: Some of the hessian diagonals cannot be square-rooted; stderr = NA is being returned")
-      }
-    }    
-    paramvec.hessian =  MARSSvectorizeparam(MLEobj.hessian)
-    #this is in Cholesky transformed form
-    vector.par.upCI = paramvec.hessian + qnorm(1-alpha/2)*vector.par.se
-    vector.par.lowCI = paramvec.hessian - qnorm(1-alpha/2)*vector.par.se
-    if(any(RQV0type=="diagonal")){ 
-      #back transform the standard error to be on the original (not chol) scale
-      for(elem in c("Q","R","V0")[RQV0type=="diagonal"]){
-        loc=grepl(elem, names(vector.par.upCI)) #where the parameter is in the vec
-        tmp=MARSShessian.chol.backtrans(MLEobj.hessian, vector.par.upCI)
-        vector.par.upCI[loc] = tmp[loc]
-        tmp=MARSShessian.backtrans(MLEobj.hessian, vector.par.lowCI)
-        vector.par.lowCI[loc] = tmp[loc]
-        #The inverse hessian we have is for the sqrt(var); we need the variance of the square of this
-        #since the elem is diagonal, we can do the following
-        var.elem=vector.par.se[loc]^2
-        M1=MLEobj.hessian$parMean[loc] #first moment
-        M2=M1^2+var.elem #second moment
-        M4=M1^4+6*M1^2*var.elem+3*var.elem^2 #fourth moment
-        vector.par.se[loc] = sqrt(M4-M2^2) #variance of elem^2 is M4-M2^2
-      }
-    }
-    if(any(RQV0type=="unconstrained")){ #need to back transform with simulation
-      for(elem in c("Q","R","V0")[RQV0type=="unconstrained"]){
-        loc=grepl(elem, names(vector.par.upCI)) #where the parameter is in the vec
-        Mu=MLEobj.hessian$parMean
-        Sigma=MLEobj.hessian$parSigma
-        a=mvrnorm(nboot,Mu,Sigma)
-        elem.boot=matrix(NA,nboot,sum(loc))
-        for(i in 1:nboot){
-          tmp.MLE=MARSSvectorizeparam(MLEobj.hessian,a[i,])
-          elem.chol=coef(tmp.MLE,type="matrix",form="marss")[[elem]]
-          elem.tmp=elem.chol%*%t(elem.chol)
-          elem.boot[i,]=elem.tmp[lower.tri(elem.tmp,diag=TRUE)]
-        }
-        tmp=apply(elem.boot,2,quantile,.975)
-        vector.par.upCI[loc] = tmp
-        tmp=apply(elem.boot,2,quantile,.025)
-        vector.par.lowCI[loc] = tmp
-        tmp=apply(elem.boot,2,var)
-        vector.par.se[loc] = sqrt(tmp)
-      }
-    }
-    vector.par.se[ na.diag ] = NA
-    vector.par.lowCI[ na.diag ] = NA
-    vector.par.upCI[ na.diag ] = NA
-    vector.par.se[ na.diag ] = NA
-    vector.par.bias = NULL; par.CI.nboot = NULL
-  } #if method hessian-numerical
   
-  if(method=="hessian-harvey1989")  {
-    #Run Harvey 1989 recursion to get the hessian of the neg LL(observed FI matrix)
-    MLEobj.hessian = MARSSFisherI(MLEobj, method="Harvey1989")
-    #deal with NAs in the Hessian
-    na.diag = is.na(diag(MLEobj.hessian))
-    diag(MLEobj.hessian)[ na.diag ] = 1 #this will lead to NA as needed
-    MLEobj.hessian[is.na(MLEobj.hessian)]=0
+  paramnames=names(paramvec)
+  
+  #default
+  if(method=="hessian"){
+    MLEobj = MARSShessian(MLEobj, method=hessian.fun)
     
-    #standard errors
-    hessInv = try(solve(MLEobj.hessian))
     vector.par.se =rep(NA,length(paramvec))
-    if(inherits(hessInv, "try-error") ) {
-      warning("MARSSparamCIs: Hessian cannot be inverted; stderr = NA is being returned")
+    if(is.null(MLEobj$parSigma)) {
+      warning("MARSSparamCIs: No parSigma element returned by Hessian function.  See marssMLE object errors (MLEobj$errors)")
     }else{
-      diag(hessInv)[ na.diag ] = 0
-      is.neg=diag(hessInv)<0
-      vector.par.se[!is.neg] = try(sqrt(diag(hessInv)[!is.neg]), silent=TRUE)      #invert the Hessian; wrap in a try() in case it fails
+      is.neg=diag(MLEobj$parSigma)<0
+      if(any(is.neg)) warning("MARSSparamCIs: parSigma element has negative values on the diagonal.  These are replaced with NA.")
+      
+      vector.par.se[!is.neg] = try(sqrt(diag(MLEobj$parSigma)[!is.neg]), silent=TRUE)      # wrap in a try() in case it fails
       if(inherits(vector.par.se, "try-error") || any(is.nan(vector.par.se))) {
-        warning("MARSSparamCIs: Some of the hessian diagonals cannot be square-rooted; stderr = NA is being returned")
+        warning("MARSSparamCIs: Some of the Hessian diagonals cannot be square-rooted; NA is being returned")
       }
     }
-    #Harvey recursion is a hessian for non-transformed parameters
-    paramvec.hessian = paramvec
-    vector.par.upCI = paramvec.hessian + qnorm(1-alpha/2)*vector.par.se
-    vector.par.lowCI = paramvec.hessian - qnorm(1-alpha/2)*vector.par.se
-    vector.par.lowCI[ na.diag ] = NA
-    vector.par.upCI[ na.diag ] = NA
-    vector.par.se[ na.diag ] = NA
+    vector.par.upCI = paramvec + qnorm(1-alpha/2)*vector.par.se
+    vector.par.lowCI = paramvec - qnorm(1-alpha/2)*vector.par.se
     vector.par.bias = NULL; par.CI.nboot = NULL
-  } #if method hessian-harvey1989
+  } #if method hessian
   
   if(method %in% c("parametric", "innovations"))  {
     boot.params = MARSSboot(MLEobj, nboot=nboot, output="parameters", sim=method,
@@ -149,23 +48,21 @@ MARSSparamCIs = function(MLEobj, method="hessian", alpha=0.05, nboot=1000, silen
     vector.par.bias = paramvec - apply(boot.params,1,mean)
     par.CI.nboot = nboot
   }
+
+  # Finalize the output
   names(vector.par.se)=paramnames
   if(!is.null(vector.par.bias)) names(vector.par.bias)=paramnames
   names(vector.par.upCI)=paramnames
   names(vector.par.lowCI)=paramnames
   for(val in c("par.se","par.bias","par.upCI","par.lowCI")){
     tmp.vec=get(paste("vector.",val,sep=""))
-    if(!is.null(tmp.vec)) MLEobj[[val]] = MARSSvectorizeparam(MLEobj,tmp.vec)[[val]]
+    if(!is.null(tmp.vec)) MLEobj[[val]] = MARSSvectorizeparam(MLEobj,tmp.vec)[["par"]]
     else MLEobj[[val]] = NULL
   }
-  MLEobj$par.se = MARSSvectorizeparam(MLEobj,vector.par.se)[["par"]]
-  if(!is.null(vector.par.bias)) MLEobj$par.bias = MARSSvectorizeparam(MLEobj,vector.par.bias)[["par"]]
-  else MLEobj$par.bias = NULL
-  MLEobj$par.upCI = MARSSvectorizeparam(MLEobj,vector.par.upCI)[["par"]]
-  MLEobj$par.lowCI = MARSSvectorizeparam(MLEobj,vector.par.lowCI)[["par"]]
-  MLEobj$par.CI.alpha = alpha
-  MLEobj$par.CI.method = method
-  MLEobj$par.CI.nboot = par.CI.nboot
+  
+  #add on information about how the CIs were constructed
+  if(method=="hessian") MLEobj$par.CI.info = list(alpha=alpha, method=method, hessian.fun=hessian.fun) 
+  if(method%in%c("parametric","innovations")) MLEobj$par.CI.info = list(alpha=alpha, method=method, nboot=par.CI.nboot) 
   return(MLEobj)
 }
 
