8f851820bac02d9a9d12afb67676c1fbeac1ebf0
Eli Holmes <eli.holmes@noaa.gov>
2018-02-13 15:24:30 -0800

    needed object$ not MLEobj$

--- R/logLik_marssMLE.R
+++ R/logLik_marssMLE.R
@@ -7,7 +7,7 @@ logLik.marssMLE <- function (object, ...) {
     
   val = MARSSkf( object, only.logLik=TRUE, return.lag.one=FALSE )$logLik # don't use obj info, recompute ==> object$logLik
   attr(val, "nobs") <- sum(!is.na(object$model$data)) # don't use obj info since user might have updated data ==> object$samp.size
-  attr(val, "df") <- length(unlist(MLEobj$par)) # on't use obj info since user might have updated model ==> object$num.params
+  attr(val, "df") <- length(unlist(object$par)) # on't use obj info since user might have updated model ==> object$num.params
   class(val) <- "logLik"
   val
   
