6a0e0eb5fb269dd22803403933c9bed86b73110a
Eli Holmes <eli.holmes@noaa.gov>
2018-03-07 20:26:59 -0800

    added augment using form

--- R/augment_marssMLE.R
+++ R/augment_marssMLE.R
@@ -1,52 +1,26 @@
 ###############################################################################################################################################
 #  augment method for class marssMLE
 #  returns fitted values, residuals, std err of residuals and std residuals
+#  the base function is augment_marxss and is in MARSS_marxss.R
+#  augment_dfa uses the optional rotate argument
 ##############################################################################################################################################
-augment.marssMLE <- function (x, type.predict = c("observations ", "states"),
-                              interval = c("none", "confidence"), level = 0.95, ...) {
-  model=x[["model"]]
-  resids = residuals(x)
-  se.resids = sqrt(apply(resids$var.residuals,3,function(x){takediag(x)}))
-  data.dims = attr(model, "model.dims")[["y"]]
-  nn = data.dims[1]
-  TT = data.dims[2]
-  if(type.predict=="observations"){
-    data.names = attr(model, "Y.names")
-    ret = data.frame(
-      .rownames=rep(data.names,each=TT), 
-      tid=rep(1:TT,nn), 
-      y = vec(t(model$data)),
-      .fitted = vec(t(fitted(x))),
-      .se.fit = vec(t(se.resids[1:nn,,drop=FALSE])),
-      .resids = vec(t(resids$model.residuals)),
-      .std.resid = vec(t(resids$std.residuals[1:nn,,drop=FALSE]))
-    )
-    if(interval=="confidence"){
-      ret = cbind(
-        .conf.low <- qnorm(level/2)*ret$.se.fit + ret$.fitted,
-        .conf.up <- qnorm(1-level/2)*ret$.se.fit + ret$.fitted
-      )
-    }
+augment.marssMLE <- function (x, type.predict = c("observations", "states"),
+                              interval = c("none", "confidence"), conf.level = 0.95, ...) {
+  args=list(...)
+  if(length(args)!=0){
+  if(!all(names(args)%in%c("rotate"))) stop("Unknown extra argument. See ?augment.marssMLE for allowed arguments.\n")
   }
-  if(type.predict=="states"){
-    state.names = attr(model, "X.names")
-    state.dims = attr(model, "model.dims")[["x"]]
-    mm = state.dims[1]
-    ret = data.frame(
-      .rownames=rep(state.names,each=TT), 
-      tid=rep(1:TT,mm), 
-      y = vec(t(model$data)),
-      .fitted = vec(t(fitted(x))),
-      .se.fit = vec(t(se.resids[1:nn,,drop=FALSE])),
-      .resids = vec(t(resids$model.residuals)),
-      .std.resid = vec(t(resids$std.residuals[1:nn,,drop=FALSE]))
-    )
-    if(interval=="confidence"){
-      ret = cbind(
-        .conf.low <- qnorm(level/2)*ret$.se.fit + ret$.fitted,
-        .conf.up <- qnorm(1-level/2)*ret$.se.fit + ret$.fitted
-      )
-    }
+  
+  type.predict = match.arg(type.predict)
+  interval = match.arg(interval)
+
+  form=attr(x[["model"]], "form")
+  augment.fun = paste("augment_", form[1], sep="")
+  tmp=try(exists(augment.fun, mode="function"),silent=TRUE)
+  if(isTRUE(tmp)){
+    ret=eval(call(augment.fun, x, type.predict = type.predict, interval = interval, conf.level = conf.level, extra=args))
+  }else{ 
+    ret=c(msg, paste("No augment_", form[1], " is available.\n", sep=""))
   }
   ret
 }
