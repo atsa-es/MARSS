bd8ab46036f2058350d5987f3d8e38d88f396ca5
Eli Holmes <eli.holmes@noaa.gov>
2018-03-07 20:27:38 -0800

    added augment

--- R/MARSS_marxss.R
+++ R/MARSS_marxss.R
@@ -1182,3 +1182,59 @@ if(length(msg) == 0){ return(NULL)
   return(msg)
 }
 }
+
+###############################################################################################################################################
+#  augment method for class marssMLE form marxss
+#  returns fitted values, residuals, std err of residuals and std residuals
+#  the other forms use this
+##############################################################################################################################################
+augment_marxss <- function (x, type.predict, interval, conf.level, extra) {
+  # rotate means to rotate the Z matrix; this is used in DFA
+  # but the user is allowed to do this for other cases also
+  model=x[["model"]]
+  resids = residuals(x)
+  se.resids = sqrt(apply(resids$var.residuals,3,function(x){takediag(x)}))
+  data.dims = attr(model, "model.dims")[["y"]]
+  nn = data.dims[1]
+  TT = data.dims[2]
+  alpha = 1-conf.level
+  if(type.predict=="observations"){
+    data.names = attr(model, "Y.names")
+    ret = data.frame(
+      .rownames=rep(data.names,each=TT), 
+      tid=rep(1:TT,nn), 
+      y = vec(t(model$data)),
+      .fitted = vec(t(fitted(x, type="observations"))),
+      .se.fit = vec(t(se.resids[1:nn,,drop=FALSE])),
+      .resids = vec(t(resids$model.residuals)),
+      .std.resid = vec(t(resids$std.residuals[1:nn,,drop=FALSE]))
+    )
+    if(interval=="confidence"){
+      ret = cbind( ret,
+        .conf.low = qnorm(alpha/2)*ret$.se.fit + ret$.fitted,
+        .conf.up = qnorm(1-alpha/2)*ret$.se.fit + ret$.fitted
+      )
+    }
+  }
+  if(type.predict=="states"){
+    state.names = attr(model, "X.names")
+    state.dims = attr(model, "model.dims")[["x"]]
+    mm = state.dims[1]
+    ret = data.frame(
+      .rownames=rep(state.names,each=TT), 
+      tid=rep(1:TT,mm), 
+      xtT = vec(t(x[["states"]])),
+      .fitted = vec(t(fitted(x, type="states"))),
+      .se.fit = vec(t(se.resids[(nn+1):(nn+mm),,drop=FALSE])),
+      .resids = vec(t(resids$model.residuals)),
+      .std.resid = vec(t(resids$std.residuals[(nn+1):(nn+mm),,drop=FALSE]))
+    )
+    if(interval=="confidence"){
+      ret = cbind( ret,
+        .conf.low = qnorm(alpha/2)*ret$.se.fit + ret$.fitted,
+        .conf.up = qnorm(1-alpha/2)*ret$.se.fit + ret$.fitted
+      )
+    }
+  }
+  ret
+}
