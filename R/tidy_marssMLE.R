cf0a2128f838f612e00d8e88935cf430ff803dc5
Eli Holmes <eli.holmes@noaa.gov>
2018-03-29 20:15:41 -0700

    partial fix.  broken still

--- R/tidy_marssMLE.R
+++ R/tidy_marssMLE.R
@@ -10,30 +10,33 @@ tidy.marssMLE = function (x,  type = c("parameters", "states"),
   extras=list()
   
   rerun.MARSSparamCIs = FALSE
-  model.has.cis = all(c("par.se", "par.lowCI", "par.upCI")%in%names(x))
-  if(conf.int) rerun.MARSSparamCIs = ifelse(model.has.cis, FALSE, TRUE)
+  model.has.cis = all(c("par.se", "par.lowCI", "par.upCI", "parSigma")%in%names(x))
+  if(conf.int & type=="parameters") rerun.MARSSparamCIs = ifelse(model.has.cis, FALSE, TRUE)
   if(!missing(...)){
     extras=list(...)
     if(!all(names(extras)%in%c("rotate", "method", "hessian.fun", "nboot"))) stop("Unknown extra argument. See ?tidy.marssMLE for allowed arguments.\n")
   }
   
-  if(conf.int & (!missing(...) | !missing(conf.level))){
+  if(type=="parameters" & conf.int & (!missing(...) | !missing(conf.level))){
     if(model.has.cis) warning("tidy.marssMLE: Your marssMLE object has par.se and CIs, but you have passed in arguments for calculating CIs.  MARSSparamCIs() will be re-run with these values.\n")
     rerun.MARSSparamCIs = TRUE
   }
   
   #set rotate
-  if(!(form%in%c("marss","marxss","dfa")))  stop("tidy.marssMLE: Allowed forms are marss, marxss, and dfa.\n")
+  if(!(form%in%c("marss","marxss","dfa")))  stop("tidy.marssMLE: Allowed forms are marss, marxss, and dfa.\n", call.=FALSE)
   rotate = FALSE
   if(form=="dfa" & "rotate"%in%names(extras)){
     rotate=extras[["rotate"]]
-    if(!(rotate %in% c(TRUE, FALSE))) stop("tidy.marssMLE: rotate must be TRUE/FALSE. \n")
+    if(!(rotate %in% c(TRUE, FALSE))) stop("tidy.marssMLE: rotate must be TRUE/FALSE. \n", call.=FALSE)
   }
-  
+  if(form!="dfa" & "rotate"%in%names(extras)) stop("tidy.marssMLE: rotate only makes sense if form='dfa'.\n  Pass in form='dfa' if your model is a DFA model, but the form\n attribute is not set (because you set up your DFA model manually). \n", call.=FALSE)
+    
   if(type=="parameters"){
   ests = coef(x, type="vector")
-  if(length(ests)==0) stop("tidy.marssMLE: No estimated parameters in your fitted model.\n")
-  
+  if(length(ests)==0) stop("tidy.marssMLE: No estimated parameters in your fitted model.\n", call.=FALSE)
+  if(form=="dfa" & rotate & length(x[["par"]][["Z"]])!=0){
+    stop("tidy.marssMLE: You are requesting the parameters for a DFA \n and requested that the Z matrix be rotated. You need to do the rotation yourself.  See ?tidy.marssMLE for the code.\n", call.=FALSE)
+  }else{
   ret = data.frame(
     term = names(ests),
     estimate = ests
@@ -46,6 +49,7 @@ tidy.marssMLE = function (x,  type = c("parameters", "states"),
             conf.high = coef(x, type="vector", what="par.upCI")
     )
   }
+  }
   rownames(ret)=NULL
   }
   if(type=="states"){
@@ -54,12 +58,13 @@ tidy.marssMLE = function (x,  type = c("parameters", "states"),
     state.dims = attr(model, "model.dims")[["x"]]
     mm = state.dims[1]
     TT = state.dims[2]
-    if(form=="dfa" & rotate){
+    #if user specified rotate
+    if(form=="dfa" & rotate & length(x[["par"]][["Z"]])!=0){
       Z.est = coef(x, type="matrix")[["Z"]]
       H = 1
       if(ncol(Z.est)>1){
         H = solve(varimax(Z.est)[["rotmat"]])
-        x[["states"]] = H %*% x[["states"]]
+        x[["states"]] = H %*% x[["states"]] #rotated states
         TT = attr(x$model, "model.dims")[["data"]][2]
         VtT=MARSSkf(x)[["VtT"]]
         for(t in 1:TT){
