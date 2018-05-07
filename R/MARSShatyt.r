77fd28500c8b4498e5336e61b9c3dc4df59cb40d
Eli Holmes <eli.holmes@noaa.gov>
2018-05-07 15:36:18 -0700

    Could have created bugs
    some simplifying of YM matrix

--- R/MARSShatyt.r
+++ R/MARSShatyt.r
@@ -8,12 +8,13 @@ MARSShatyt = function( MLEobj ) {
   }else{ kfList=MARSSkf(MLEobj) }
   model.dims=attr(MODELobj,"model.dims")
   n=model.dims$data[1]; TT=model.dims$data[2]; m=model.dims$x[1]
-  
-  #create the YM matrix
-  YM=matrix(as.numeric(!is.na(MODELobj[["data"]])),n,TT)
-  #Make sure the missing vals in y are zeroed out if there are any
+
   y=MODELobj$data
-  y[YM==0]=0
+  #create the YM matrix; need to keep track of location of missing
+  #YM=matrix(as.numeric(!is.na(y)),n,TT) # TRUE is present ; FALSE is missing
+  YM=!is.na(y)
+  #Make sure the missing vals in y are zeroed out if there are any
+  y[!YM]=0
   
   #set-up matrices for hatxt for 1:TT and 1:TT-1
   IIz=list(); IIz$V0=makediag(as.numeric(takediag(parmat(MLEobj, "V0", t=1)$V0)==0),m)
@@ -64,25 +65,26 @@ MARSShatyt = function( MLEobj ) {
         #is.R.diagonal = isDiagonal(pari$R)
       }
     }
-    if(all(YM[,t]==1)){  #none missing
+    YMt=YM[,t]
+    if(all(YMt)){  #none missing
       hatyt[,t]=y[,t,drop=FALSE]
       hatytt1[,t]=pari$Z%*%hatxtt1[,t,drop=FALSE]+pari$A
-      hatOt[,,t]=tcrossprod(hatyt[,t,drop=FALSE]) #matrix() is faster than t()
-#      hatOt[,,t]=hatyt[,t,drop=FALSE]%*%matrix(hatyt[,t,drop=FALSE],1,n) #matrix() is faster than t()
-      hatyxt[,,t]=tcrossprod(hatyt[,t,drop=FALSE], hatxt[,t,drop=FALSE])
-#      hatyxt[,,t]=hatyt[,t,drop=FALSE]%*%matrix(hatxt[,t,drop=FALSE],1,m)
-      hatyxtt1[,,t]=tcrossprod(hatyt[,t,drop=FALSE], hatxt1[,t,drop=FALSE])
-#      hatyxtt1[,,t]=hatyt[,t,drop=FALSE]%*%matrix(hatxt1[,t,drop=FALSE],1,m)
-      hatyxttp[,,t]=tcrossprod(hatyt[,t,drop=FALSE], hatxtp[,t,drop=FALSE])
+      hatOt[,,t]=base::tcrossprod(hatyt[,t,drop=FALSE]) 
+#      hatOt[,,t]=hatyt[,t,drop=FALSE]%*%t(hatyt[,t,drop=FALSE],1,n)
+      hatyxt[,,t]=base::tcrossprod(hatyt[,t,drop=FALSE], hatxt[,t,drop=FALSE])
+#      hatyxt[,,t]=hatyt[,t,drop=FALSE]%*%t(hatxt[,t,drop=FALSE],1,m)
+      hatyxtt1[,,t]=base::tcrossprod(hatyt[,t,drop=FALSE], hatxt1[,t,drop=FALSE])
+#      hatyxtt1[,,t]=hatyt[,t,drop=FALSE]%*%t(hatxt1[,t,drop=FALSE],1,m)
+      hatyxttp[,,t]=base::tcrossprod(hatyt[,t,drop=FALSE], hatxtp[,t,drop=FALSE])
     }else{
       I.2 = I.r = I.n; 
-      I.2[YM[,t]==1,]=0 #1 if YM=0 and 0 if YM=1
-      I.r[YM[,t]==0 | diag.R==0,]=0   #if Y missing or R = 0, then 0
+      I.2[YMt,]=0 #1 if YM=0 and 0 if YM=1
+      I.r[!YMt | diag.R==0,]=0   #if Y missing or R = 0, then 0
       Delta.r=I.n
       if(is.R.diagonal) Delta.r = I.n-I.r
-      if(!is.R.diagonal && any(YM[,t]==1 & diag.R!=0)){           
-        mho.r = I.r[YM[,t]==1 & diag.R!=0,,drop=FALSE]
-        t.mho.r = I.r[,YM[,t]==1 & diag.R!=0,drop=FALSE]
+      if(!is.R.diagonal && any(YMt & diag.R!=0)){           
+        mho.r = I.r[YMt & diag.R!=0,,drop=FALSE]
+        t.mho.r = I.r[,YMt & diag.R!=0,drop=FALSE]
         Rinv = try(chol(mho.r%*%pari$R%*%t.mho.r))
         #Catch errors before entering chol2inv
         if(class(Rinv)=="try-error") {
@@ -96,11 +98,12 @@ MARSShatyt = function( MLEobj ) {
       t.DZ = matrix(Delta.r%*%pari$Z,m,n,byrow=TRUE)
       hatOt[,,t]=I.2%*%(Delta.r%*%pari$R+Delta.r%*%pari$Z%*%hatVt[,,t]%*%t.DZ)%*%I.2 + tcrossprod(hatyt[,t,drop=FALSE])
 #      hatOt[,,t]=I.2%*%(Delta.r%*%pari$R+Delta.r%*%pari$Z%*%hatVt[,,t]%*%t.DZ)%*%I.2 + hatyt[,t,drop=FALSE]%*%matrix(hatyt[,t,drop=FALSE],1,n)
-      hatyxt[,,t]=tcrossprod(hatyt[,t,drop=FALSE], hatxt[,t,drop=FALSE])+Delta.r%*%pari$Z%*%hatVt[,,t]
-#      hatyxt[,,t]=hatyt[,t,drop=FALSE]%*%matrix(hatxt[,t,drop=FALSE],1,m)+Delta.r%*%pari$Z%*%hatVt[,,t]
-      hatyxtt1[,,t]=tcrossprod(hatyt[,t,drop=FALSE],hatxt1[,t,drop=FALSE])+Delta.r%*%pari$Z%*%hatVtt1[,,t]
-#      hatyxtt1[,,t]=hatyt[,t,drop=FALSE]%*%matrix(hatxt1[,t,drop=FALSE],1,m)+Delta.r%*%pari$Z%*%hatVtt1[,,t]
-      hatyxttp[,,t]=tcrossprod(hatyt[,t,drop=FALSE],hatxtp[,t,drop=FALSE])+Delta.r%*%pari$Z%*%t(hatVtpt[,,t])
+      hatyxt[,,t]=base::tcrossprod(hatyt[,t,drop=FALSE], hatxt[,t,drop=FALSE])+Delta.r%*%pari$Z%*%hatVt[,,t]
+#      hatyxt[,,t]=hatyt[,t,drop=FALSE]%*%t(hatxt[,t,drop=FALSE],1,m)+Delta.r%*%pari$Z%*%hatVt[,,t]
+      hatyxtt1[,,t]=base::tcrossprod(hatyt[,t,drop=FALSE],hatxt1[,t,drop=FALSE])+Delta.r%*%pari$Z%*%hatVtt1[,,t]
+#      hatyxtt1[,,t]=hatyt[,t,drop=FALSE]%*%t(hatxt1[,t,drop=FALSE],1,m)+Delta.r%*%pari$Z%*%hatVtt1[,,t]
+      hatyxttp[,,t]=base::tcrossprod(hatyt[,t,drop=FALSE],hatxtp[,t,drop=FALSE])+Delta.r%*%base::tcrossprod(pari$Z, hatVtpt[,,t])
+      #hatyxttp[,,t]=base::tcrossprod(hatyt[,t,drop=FALSE],hatxtp[,t,drop=FALSE])+Delta.r%*%pari$Z%*%t(hatVtpt[,,t])
     }
   } #for loop over time
   rtn.list=list(ytT = hatyt, OtT = hatOt, yxtT=hatyxt, yxt1T=hatyxtt1, yxttpT = hatyxttp, ytt1 = hatytt1) 
