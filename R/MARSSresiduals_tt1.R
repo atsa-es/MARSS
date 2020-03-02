93a0d56883aae18452ef969a0979ddd8d6f68535
Eli Holmes <eli.holmes@noaa.gov>
2020-03-02 15:37:34 -0800

    bug: printed error for residuals always

--- R/MARSSresiduals_tt1.R
+++ R/MARSSresiduals_tt1.R
@@ -94,7 +94,7 @@ MARSSresiduals.tt1 <- function(object, method=c("SS"), normalize = FALSE, silent
   rownames(et) <- rownames(st.et) <- rownames(var.et) <- colnames(var.et) <- Y.names
   
   # output any warnings
-  if(object[["control"]][["trace"]] >= 0 & !silent) cat("MARSSresiduals.tT reported warnings. See msg element of returned residuals object.\n")
+  if(!is.null(msg) && object[["control"]][["trace"]] >= 0 & !silent) cat("MARSSresiduals.tT reported warnings. See msg element of returned residuals object.\n")
 
   return(list(residuals = et, std.residuals = st.et, mar.residuals = mar.st.et, var.residuals = var.et, msg = msg ))
 }
