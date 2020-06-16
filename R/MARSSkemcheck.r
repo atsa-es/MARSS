8102c367b4d80b791918f52aa621ab398250c581
EEH <eli.holmes@noaa.gov>
2020-06-16 09:40:24 -0700

    fix eigenvalue outside unit circle error msg

--- R/MARSSkemcheck.r
+++ R/MARSSkemcheck.r
@@ -27,7 +27,7 @@ MARSSkemcheck <- function(MLEobj) {
       tmp.MLEobj <- list(marss = MODELobj, par = list(B = tmpparB)) # B is fixed but par might have cols from other times
       parB <- parmat(tmp.MLEobj, "B", t = t)$B
       if (!is.unitcircle(parB)) {
-        msg <- c(msg, " All the eigenvalues of B must be within the unit circle: all(sqrt(Re(eigen(B)$values)^2+Im(eigen(B)$values)^2)<=1)\n")
+        msg <- c(msg, " All the eigenvalues of B must be within the unit circle: all(abs(eigen(B)$values)<=1)\n")
         ok <- FALSE
       }
     }
