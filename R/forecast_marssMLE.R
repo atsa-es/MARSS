bb5386e0e5d89eb7cc9a04419a62c3a6c5f51bdf
EEH <eli.holmes@noaa.gov>
2020-07-15 21:57:30 -0700

    make outlist match that of predict

--- R/forecast_marssMLE.R
+++ R/forecast_marssMLE.R
@@ -139,13 +139,18 @@ forecast.marssMLE <- function(object, h=10,
   outlist <- list(
     method = c("MARSS", object[["method"]]),
     model = object,
-    newdata = newdata,
     level = 100*conf.level,
-    pred = ret,
     type = type,
+    pred = ret,
     t = 1:(TT+h),
-    h = h
+    h = h,
+    n.ahead = h,
+    x0 = coef(object, type = "matrix")[["x0"]],
+    tinitx = object[["model"]][["tinitx"]],
+    newdata = newdata
   )
+  tmp <- colnames(outlist[["pred"]])
+  colnames(outlist[["pred"]])[which(tmp == ".sd")] <- "se"
   
   class(outlist) <- "marssPredict"
   
