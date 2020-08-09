eb4ee4be35bd1e894af2de2c91055a8d83f73ea1
EEH <eli.holmes@noaa.gov>
2020-08-08 18:55:28 -0700

    add tt residuals

--- R/MARSSresiduals.R
+++ R/MARSSresiduals.R
@@ -1,6 +1,7 @@
-MARSSresiduals <- function(object, ..., type=c("tT", "tt1"), normalize = FALSE, silent=FALSE) {
+MARSSresiduals <- function(object, ..., type=c("tT", "tt1", "tt"), normalize = FALSE, silent=FALSE) {
   type <- match.arg(type)
   if(type=="tT") val <- MARSSresiduals.tT(object, ..., normalize=normalize, silent=silent)
   if(type=="tt1") val <- MARSSresiduals.tt1(object, ..., normalize=normalize, silent=silent)
+  if(type=="tt") val <- MARSSresiduals.tt(object, ..., normalize=normalize, silent=silent)
   return(val)
 }
