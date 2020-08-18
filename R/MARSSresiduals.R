2764bef9ddd7b1612fe0ac3bc98268d5339f147d
EEH <eli.holmes@noaa.gov>
2020-08-17 20:13:04 -0700

    no warning about fun.kf

--- R/MARSSresiduals.R
+++ R/MARSSresiduals.R
@@ -1,7 +1,6 @@
 MARSSresiduals <- function(object, ..., type=c("tT", "tt1", "tt"), normalize = FALSE, silent=FALSE, fun.kf = c("MARSSkfas", "MARSSkfss")) {
   type <- match.arg(type)
   fun.kf <- match.arg(fun.kf)
-  if(type!="tT" & fun.kf=="MARSSkfas") message("fun.kf must be MARSSkfss for type='tt' or 'tt1'. fun.kf reset to MARSSkfss.\n")
   if(type=="tT") val <- MARSSresiduals.tT(object, ..., normalize=normalize, silent=silent, fun.kf=fun.kf)
   if(type=="tt1") val <- MARSSresiduals.tt1(object, ..., normalize=normalize, silent=silent)
   if(type=="tt") val <- MARSSresiduals.tt(object, ..., normalize=normalize, silent=silent)
