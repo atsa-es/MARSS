a8f3cde2c9fdc1dfb2a918197171ab5ab948cba5
EEH <eli.holmes@noaa.gov>
2020-10-02 09:42:51 -0700

    Add silent for no user interaction

--- tests/testthat/test-plotting.R
+++ tests/testthat/test-plotting.R
@@ -11,23 +11,23 @@ for(i in model.list){
 fit <- MARSS(i$data, model=i$model, control=i$control, silent=TRUE, method=method)
 if(fit$convergence==54) next
 fit2 <- fit; fit2$fun.kf="MARSSkfas"
-  p1 <- try(plot(fit2), silent=TRUE)
+  p1 <- try(plot(fit2, silent=TRUE), silent=TRUE)
   test_that("plot fit2", {
     expect_true(!inherits(p1, "try-error"))
   })
   library(ggplot2)
-  p1 <- try(autoplot(fit2), silent=TRUE)
+  p1 <- try(autoplot(fit2, silent=TRUE), silent=TRUE)
   test_that("plot fit2", {
     expect_true(!inherits(p1, "try-error"))
   })
   
   if(!i$kfss) next
   fit1 <- fit; fit1$fun.kf="MARSSkfss"
-  p1 <- try(plot(fit1), silent=TRUE)
+  p1 <- try(plot(fit1, silent=TRUE), silent=TRUE)
   test_that("plot fit1", {
     expect_true(!inherits(p1, "try-error"))
   })
-  p1 <- try(autoplot(fit1), silent=TRUE)
+  p1 <- try(autoplot(fit1, silent=TRUE), silent=TRUE)
   test_that("plot fit1", {
     expect_true(!inherits(p1, "try-error"))
   })
