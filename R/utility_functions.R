c1f34e57869ac2fe38577a68217f77926bde3b70
Eli Holmes <eli.holmes@noaa.gov>
2021-09-27 09:50:08 -0700

    add match.arg.exact()
    #128

--- R/utility_functions.R
+++ R/utility_functions.R
@@ -1007,3 +1007,38 @@ ldiag <- function(x, nrow = NA) {
 MARSS.out <- function() {
   # Null function for man file
 }
+
+match.arg.exact <- function (arg, choices, several.ok = FALSE, exact = TRUE) 
+{
+  if (missing(choices)) {
+    formal.args <- formals(sys.function(sysP <- sys.parent()))
+    choices <- eval(formal.args[[as.character(substitute(arg))]], 
+                    envir = sys.frame(sysP))
+  }
+  if (is.null(arg)) 
+    return(choices[1L])
+  else if (!is.character(arg)) 
+    stop("'arg' must be NULL or a character vector")
+  if (!several.ok) {
+    if (identical(arg, choices)) 
+      return(arg[1L])
+    if (length(arg) > 1L) 
+      stop("'arg' must be of length 1")
+  }
+  else if (length(arg) == 0L) 
+    stop("'arg' must be of length >= 1")
+  # HERE IS THE MODIFIED CODE
+  if (exact)
+    i <- match(arg, choices, nomatch = 0L)
+  else
+    i <- pmatch(arg, choices, nomatch = 0L, duplicates.ok = TRUE)
+  # END MODIFIED CODE
+  
+  if (all(i == 0L)) 
+    stop(gettextf("'arg' should be one of %s", paste(dQuote(choices), 
+                                                     collapse = ", ")), domain = NA)
+  i <- i[i > 0L]
+  if (!several.ok && length(i) > 1) 
+    stop("there is more than one match in 'match.arg'")
+  choices[i]
+}
