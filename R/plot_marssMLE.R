d53718db487f0308ec26c77bba09bdf28f69ecc0
Eli Holmes <eli.holmes@noaa.gov>
2021-10-11 13:56:54 -0700

    make plot plots look like autoplot plots
    #60

--- R/plot_marssMLE.R
+++ R/plot_marssMLE.R
@@ -1,9 +1,9 @@
 plot.marssMLE <-
   function(x,
            plot.type = c(
-             "fitted.ytT", "fitted.ytt", "fitted.ytt1", 
+             "fitted.ytT", "fitted.ytt", "fitted.ytt1",
              "ytT", "ytt", "ytt1",
-             "fitted.xtT", "fitted.xtt1", 
+             "fitted.xtT", "fitted.xtt1",
              "xtT", "xtt", "xtt1",
              "model.resids.ytt1", "qqplot.model.resids.ytt1", "acf.model.resids.ytt1",
              "std.model.resids.ytt1", "qqplot.std.model.resids.ytt1", "acf.std.model.resids.ytt1",
@@ -16,6 +16,7 @@ plot.marssMLE <-
              "residuals", "all"
            ),
            form = c("marxss", "marss", "dfa"),
+           standardization = c("Cholesky", "marginal", "Block.Cholesky"),
            conf.int = TRUE, conf.level = 0.95, decorate = TRUE, pi.int = FALSE,
            plot.par = list(),
            ..., silent = FALSE) {
@@ -26,7 +27,7 @@ plot.marssMLE <-
     # Argument checks
     standardization <- match.arg(standardization)
 
-        # Argument checks: plot.type
+    # Argument checks: plot.type
     if (missing(plot.type)) {
       plot.type <- c(
         "fitted.ytT", "xtT",
@@ -43,12 +44,12 @@ plot.marssMLE <-
       )
     }
     plot.all <- FALSE
-    if (identical(plot.type,"all")){
+    if (identical(plot.type, "all")) {
       plot.all <- TRUE
       plot.type <- eval(formals()$plot.type)
       plot.type <- plot.type[!(plot.type %in% c("residuals", "all"))]
     }
-    
+
     if (!is.numeric(conf.level) || length(conf.level) > 1 || conf.level > 1 || conf.level < 0) stop("plot.marssMLE: conf.level must be a single number between 0 and 1.", call. = FALSE)
     if (!(conf.int %in% c(TRUE, FALSE))) stop("plot.marssMLE: conf.int must be TRUE/FALSE", call. = FALSE)
 
@@ -62,8 +63,7 @@ plot.marssMLE <-
     plotpar <- list(
       point.pch = 19, point.col = "blue", point.fill = "blue", point.size = 1,
       line.col = "black", line.size = 1, line.linetype = "solid",
-      ci.fill = "grey70", ci.col = "grey70", ci.linetype = "solid",
-      ci.linesize = 0, ci.alpha = 0.6
+      ci.col = "grey70", ci.border = NA, ci.lwd = 1, ci.lty = 1
     )
     if (!is.list(plot.par)) stop("autoplot.marssMLE: plot.par must be a list.", call. = FALSE)
     if (!missing(plot.par)) {
@@ -75,16 +75,20 @@ plot.marssMLE <-
         }
       }
     }
-    
+
     # Check class and alter plot.type as needed
     if (!inherits(x, "marssMLE")) {
-      if(inherits(x, "marssResiduals")){
+      if (inherits(x, "marssResiduals")) {
         plot.type <- plot.type[stringr::str_detect(plot.type, "resids")]
         # Make sure that plot types are possible for the object that the user passed in
         ctype <- unique(x$type)
-        plot.type <- plot.type[sapply(plot.type, function(x){any(stringr::str_detect(x, ctype))})]
+        plot.type <- plot.type[sapply(plot.type, function(x) {
+          any(stringr::str_detect(x, ctype))
+        })]
         cname <- unique(x$name)
-        plot.type <- plot.type[sapply(plot.type, function(x){any(stringr::str_detect(x, cname))})]
+        plot.type <- plot.type[sapply(plot.type, function(x) {
+          any(stringr::str_detect(x, cname))
+        })]
         if (length(plot.type) == 0) {
           message("Nothing to plot. Either your MARSSresiduals object does not include model or state residuals or you have passed in the wrong plot.type, i.e. model residual plots when your MARRSSresiduals object only includes state residuals.")
           return()
@@ -92,11 +96,11 @@ plot.marssMLE <-
         # Set up the residuals object
         resids <- x
         cstan <- attr(resids, "standardization")
-      }else{
+      } else {
         stop("plot.marssMLE: x must be class marssMLE or marssResiduals.", call. = FALSE)
       }
     }
-    
+
     extras <- list()
     if (!missing(...)) {
       extras <- list(...)
@@ -113,7 +117,7 @@ plot.marssMLE <-
     alpha <- 1 - conf.level
 
     # If user requests any residuals plots, set up the residuals data frames unless x is marssResiduals object
-    if(!inherits(x, "marssResiduals")){
+    if (!inherits(x, "marssResiduals")) {
       resids <- c()
       cstan <- standardization
       if (any(stringr::str_detect(plot.type, "tt1"))) {
@@ -126,7 +130,7 @@ plot.marssMLE <-
         resids <- rbind(resids, residuals.marssMLE(x, type = "tT", standardization = cstan))
       }
     }
-    
+
     fitted.plots <- paste0("fitted.", c("ytt1", "ytt", "ytT", "xtT", "xtt1"))
     for (i in fitted.plots[fitted.plots %in% plot.type]) {
       ctype <- rev(stringr::str_split(i, "[.]")[[1]])[1]
@@ -135,7 +139,7 @@ plot.marssMLE <-
       df <- fitted.marssMLE(x, type = ctype, interval = "confidence", level = conf.level)
       df$ymin <- df$.conf.low
       df$ymax <- df$.conf.up
-      if(stringr::str_detect(i, "x")) df$y <- df$.x
+      if (stringr::str_detect(i, "x")) df$y <- df$.x
       if (pi.int) {
         df2 <- fitted.marssMLE(x, type = ctype, interval = "prediction", level = conf.level)
         df$ymin.pi <- df2$.lwr
@@ -153,7 +157,7 @@ plot.marssMLE <-
           ylims <- c(min(.fitted, y, ymin, ymax, na.rm = TRUE), max(.fitted, y, ymin, ymax, na.rm = TRUE))
           plot(t, .fitted, type = "l", xlab = "", ylab = "Estimate", ylim = ylims)
           title(tit)
-          if (conf.int) polygon(c(t, rev(t)), c(ymin, rev(ymax)), col = plotpar$ci.col, border = plotpar$ci.border)
+          if (conf.int) polygon(c(t, rev(t)), c(ymin, rev(ymax)), col = plotpar$ci.col, border = plotpar$ci.border, lwd = plotpar$ci.lwd, lty = plotpar$ci.lty)
           if (decorate) points(t, y, col = plotpar$point.col, pch = plotpar$point.pch, cex = plotpar$point.size)
           lines(t, .fitted, col = plotpar$line.col, lwd = plotpar$line.lwd)
           if (pi.int) {
@@ -178,7 +182,7 @@ plot.marssMLE <-
     for (i in state.plots[state.plots %in% plot.type]) {
       ctype <- i
 
-      if ("rotate" %in% names(extras)) {
+      if (model_form == "dfa" && "rotate" %in% names(extras)) {
         rotate <- extras[["rotate"]]
         if (!(rotate %in% c(TRUE, FALSE))) stop("plot.marssMLE: rotate must be TRUE/FALSE. \n")
       } else {
@@ -186,17 +190,9 @@ plot.marssMLE <-
       }
 
       states <- tsSmooth.marssMLE(x, type = ctype, interval = ifelse(conf.int, "confidence", "none"), level = conf.level, ...)
-      if (model_form == "dfa") {
-        if (rotate) {
-          rottext <- "rotated"
-        } else {
-          rottext <- ""
-        }
-        states$.rownames <- paste("DFA", rottext, "trend", states$.rownames)
-      } else {
-        states$.rownames <- paste0("State ", states$.rownames)
+      if (rotate) {
+        states$.rownames <- paste("Rotated", states$.rownames)
       }
-
       nX <- min(9, attr(x$model, "model.dims")$x[1])
       plot.nrow <- round(sqrt(nX))
       plot.ncol <- ceiling(nX / plot.nrow)
@@ -206,7 +202,7 @@ plot.marssMLE <-
           ylims <- c(min(.estimate, .conf.low, na.rm = TRUE), max(.estimate, .conf.up, na.rm = TRUE))
           plot(t, .estimate, type = "l", xlab = "", ylab = "Estimate", ylim = ylims)
           title(plt)
-          if (conf.int) polygon(c(t, rev(t)), c(.conf.low, rev(.conf.up)), col = plotpar$ci.col, border = plotpar$ci.border)
+          if (conf.int) polygon(c(t, rev(t)), c(.conf.low, rev(.conf.up)), col = plotpar$ci.col, border = plotpar$ci.border, lwd = plotpar$ci.lwd, lty = plotpar$ci.lty)
           lines(t, .estimate)
           box()
         })
@@ -221,45 +217,53 @@ plot.marssMLE <-
       }
     }
 
-    resids.vs.time.plots <- paste0(rep(c("model.resids", "std.model.resids"), each = 3), c(".ytT", ".ytt", ".ytt1"))
+    resids.vs.time.plots <- c(paste0(rep(c("model.resids", "std.model.resids"), each = 3), c(".ytT", ".ytt", ".ytt1")), "state.resids.xtT", "std.state.resids.xtT")
     for (i in resids.vs.time.plots[resids.vs.time.plots %in% plot.type]) {
       ctype <- rev(stringr::str_split(i, "[.]")[[1]])[1] # ytT, ytt or ytt1
       cname <- ifelse(stringr::str_detect(i, "model"), "model", "state")
       df <- subset(resids, resids$type == ctype)
       if (stringr::str_detect(i, "std")) df$.resids <- df$.std.resids
       df$.resids[is.na(df$value)] <- NA
-      nY <- min(9, attr(x$model, "model.dims")$y[1])
+      # need to adjust sigma for making a polygon
+      if (stringr::str_detect(i, "std")) {
+        df$.sigma <- 1
+      } else {
+        df$.sigma[is.na(df$value)] <- 0
+      }
+      nY <- min(9, attr(x$model, "model.dims")[[stringr::str_sub(ctype, 1, 1)]][1])
       plot.ncol <- round(sqrt(nY))
       plot.nrow <- ceiling(nY / plot.ncol)
       par(mfrow = c(plot.nrow, plot.ncol), mar = c(2, 4, 2, 1) + 0.1)
       for (plt in unique(df$.rownames)) {
         with(subset(df, df$.rownames == plt), {
-          ylims <- c(min(.resids, na.rm = TRUE), max(.resids, na.rm = TRUE))
+          if (conf.int) {
+            ymin <- qnorm(alpha / 2) * .sigma
+            ymax <- -qnorm(alpha / 2) * .sigma
+            ylims <- c(min(.resids, ymin, na.rm = TRUE), max(.resids, ymax, na.rm = TRUE))
+          } else {
+            ylims <- c(min(.resids, na.rm = TRUE), max(.resids, na.rm = TRUE))
+          }
           plot(t, .resids,
             type = "p", xlab = "",
             ylab = "", ylim = ylims,
             col = plotpar$point.col, pch = plotpar$point.pch,
             cex = plotpar$point.size
           )
+          if (conf.int) {
+            polygon(c(t, rev(t)), c(ymin, rev(ymax)), col = plotpar$ci.col, border = plotpar$ci.border, lwd = plotpar$ci.lwd, lty = plotpar$ci.lty)
+            points(t, .resids, col = plotpar$point.col, pch = plotpar$point.pch, cex = plotpar$point.size)
+          }
           if (decorate) {
             lo <- predict(loess(.resids ~ t), newdata = data.frame(t = t), se = TRUE)
             lo.t <- names(lo$fit)
-            if (stringr::str_detect(i, "std")) {
-              sigma <- 1
-            } else {
-              sigma <- .sigma
-              sigma[is.na(value)] <- 0
-            }
-            ymin <- qnorm(alpha / 2) * sigma
-            ymax <- -qnorm(alpha / 2) * sigma
-            ylims <- c(min(.resids, ymin, na.rm = TRUE), max(.resids, ymax, na.rm = TRUE))
             lines(t, lo$fit, col = plotpar$line.col, lwd = plotpar$line.lwd)
           }
           title(plt)
           box()
           abline(h = 0, lty = 3)
         })
-        mtext(ifelse(stringr::str_detect(i, "std"), "Standardized observation residuals, y - E[y]", "Observation residuals, y - E[y]"), side = 2, outer = TRUE, line = -1)
+        if (cname == "model") mtext(ifelse(stringr::str_detect(i, "std"), "Standardized observation residuals, y - E[y]", "Observation residuals, y - E[y]"), side = 2, outer = TRUE, line = -1)
+        if (cname == "state") mtext(ifelse(stringr::str_detect(i, "std"), "Standardized state residuals, xtT - E[x]", "State residuals, xtT - E[x]"), side = 2, outer = TRUE, line = -1)
       }
       plot.type <- plot.type[plot.type != i]
       if (!silent) cat(paste0("plot type = ", i, "\n"))
@@ -270,56 +274,6 @@ plot.marssMLE <-
         }
       }
     }
-
-    resids.vs.time.plots <- c("state.resids.xtT", "std.state.resids.xtT")
-    for (i in resids.vs.time.plots[resids.vs.time.plots %in% plot.type]) {
-      # make plot of process residuals; set form='marxss' to get process resids
-      ctype <- rev(stringr::str_split(i, "[.]")[[1]])[1]
-      cname <- ifelse(stringr::str_detect(i, "model"), "model", "state")
-      df <- subset(resids, resids$type == ctype)
-      if (stringr::str_detect(i, "std")) df$.resids <- df$.std.resids
-      df$.rownames <- paste0("State ", df$.rownames)
-      nX <- min(9, attr(x$model, "model.dims")$x[1])
-      plot.nrow <- round(sqrt(nX))
-      plot.ncol <- ceiling(nX / plot.nrow)
-      par(mfrow = c(plot.nrow, plot.ncol), mar = c(2, 4, 2, 1) + 0.1)
-      for (plt in unique(df$.rownames)) {
-        with(subset(df, df$.rownames == plt), {
-          ylims <- c(min(.resids, na.rm = TRUE), max(.resids, na.rm = TRUE))
-          plot(t, .resids,
-            type = "p", xlab = "",
-            ylab = "", ylim = ylims,
-            col = plotpar$point.col, pch = plotpar$point.pch,
-            cex = plotpar$point.size
-          )
-          if (decorate) {
-            lo <- predict(loess(.resids ~ t), newdata = data.frame(t = t), se = TRUE)
-            lo.t <- names(lo$fit)
-            sigma <- .sigma
-            ymin <- qnorm(alpha / 2) * sigma
-            ymax <- -qnorm(alpha / 2) * sigma
-            ylims <- c(min(.resids, ymin, na.rm = TRUE), max(.resids, ymax, na.rm = TRUE))
-            lines(t, lo$fit, col = plotpar$line.col, lwd = plotpar$line.lwd)
-          }
-
-          title(plt)
-          box()
-          abline(h = 0, lty = 3)
-        })
-        mtext(ifelse(stringr::str_detect(i, "std"), "Standardized state residuals, xtT - E[x]", "State residuals, xtT - E[x]"),
-          side = 2, outer = TRUE, line = -1
-        )
-      }
-      plot.type <- plot.type[plot.type != i]
-      if (!silent) cat(paste0("plot type = ", i, "\n"))
-      if (length(plot.type) != 0 && !silent) {
-        ans <- readline(prompt = "Hit <Return> to see next plot (q to exit): ")
-        if (tolower(ans) == "q") {
-          return()
-        }
-      }
-    }
-
 
     # QQplots for normality
     slp <- function(yy) {
@@ -367,16 +321,16 @@ plot.marssMLE <-
     y.plots <- c("ytT", "ytt", "ytt1")
     for (i in y.plots[y.plots %in% plot.type]) {
       ctype <- i
-      if ( ctype %in% c("ytT", "ytt1") | !conf.int){
+      if (ctype %in% c("ytT", "ytt1") | !conf.int) {
         df <- tsSmooth.marssMLE(x, type = i, ifelse(conf.int, "confidence", "none"), level = conf.level)
-      }else{
-        if( plot.all ) next # If plot.type="all" then just skip the problematic plots
-        if( conf.int ) stop(paste("Confidence intervals for", i, "are not yet implemented in MARSS.\nPass in conf.int=FALSE to autoplot()."))
+      } else {
+        if (plot.all) next # If plot.type="all" then just skip the problematic plots
+        if (conf.int) stop(paste("Confidence intervals for", i, "are not yet implemented in MARSS.\nPass in conf.int=FALSE to autoplot()."))
       }
       if (conf.int) {
         df$ymin <- df$.conf.low
         df$ymax <- df$.conf.up
-      }else{
+      } else {
         df$ymin <- df$y
         df$ymax <- df$y
       }
@@ -391,7 +345,7 @@ plot.marssMLE <-
           ylims <- c(min(.estimate, y, ymin, ymax, na.rm = TRUE), max(.estimate, y, ymin, ymax, na.rm = TRUE))
           plot(t, .estimate, type = "l", xlab = "", ylab = "Estimate", ylim = ylims)
           title(tit)
-          if (conf.int) polygon(c(t, rev(t)), c(ymin, rev(ymax)), col = plotpar$ci.col, border = plotpar$ci.border)
+          if (conf.int) polygon(c(t, rev(t)), c(ymin, rev(ymax)), col = plotpar$ci.col, border = plotpar$ci.border, lwd = plotpar$ci.lwd, lty = plotpar$ci.lty)
           points(t, y, col = plotpar$point.col, pch = plotpar$point.pch, cex = plotpar$point.size)
           lines(t, .estimate, col = plotpar$line.col, lwd = plotpar$line.lwd)
           box()
