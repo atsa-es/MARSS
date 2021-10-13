4ac01e6b32ab58df65a503f14289d13c8f6aa69f
Eli Holmes <eli.holmes@noaa.gov>
2021-10-13 12:42:37 -0700

    forgot that MARSS does not import stringr

--- R/plot_marssResiduals.R
+++ R/plot_marssResiduals.R
@@ -6,20 +6,20 @@ plot.marssResiduals <-
            silent = FALSE) {
     tmp <- match.arg(plot.type)
     plot.type <- eval(formals(plot.marssMLE)$plot.type)
-    plot.type <- plot.type[stringr::str_detect(plot.type, "resids")]
+    plot.type <- plot.type[grepl("resids", plot.type, fixed=TRUE)]
     ctype <- unique(x$type)
     plot.type <- plot.type[sapply(plot.type, function(x) {
-      rev(stringr::str_split(x, "[.]")[[1]])[1] %in% ctype})]
+      rev(strsplit(x, "[.]")[[1]])[1] %in% ctype})]
     cname <- unique(x$name)
     plot.type <- plot.type[sapply(plot.type, function(x) {
-      any(stringr::str_detect(x, cname))})]
+      any(sapply(cname, function(s) {grepl(s, x)}))})]
     if (tmp == "residuals") {
       plot.type <- plot.type[!sapply(plot.type, function(x) {
-        any(stringr::str_detect(x, c("qqplot", "acf")))
+        any(grepl("qqplot", x) | grepl("acf", x))
       })]
     }
-    if (tmp == "qqplot") plot.type <- plot.type[stringr::str_detect(plot.type, "qqplot")]
-    if (tmp == "acf") plot.type <- plot.type[stringr::str_detect(plot.type, "acf")]
+    if (tmp == "qqplot") plot.type <- plot.type[grepl("qqplot", plot.type)]
+    if (tmp == "acf") plot.type <- plot.type[grepl("acf", plot.type)]
 
     plot.marssMLE(x, plot.type = plot.type, 
                   conf.int = conf.int, conf.level = conf.level, 
